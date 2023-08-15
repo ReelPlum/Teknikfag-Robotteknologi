7133cb1eb84367fe896f4414d0bda57f0577548c
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-15 12:49:51 +0200

    IO pinMode on/off

--- Hello world/src/main.cpp
+++ Hello world/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int i = 0;
+// int i = 0;
 
 void setup()
 {
   Serial.begin(115200);
+  pinMode(23, OUTPUT);
 
   Serial.println("Setup exiting");
 }
@@ -13,10 +14,18 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  Serial.print("Counter: ");
-  Serial.print(i);
+  // Serial.print("Counter: ");
+  // Serial.println(i);
 
-  i = i + 1;
+  // i = i + 1;
+
+  // delay(500);
+
+  digitalWrite(13, HIGH);
+
+  delay(500);
+
+  digitalWrite(13, LOW);
 
   delay(500);
 }
