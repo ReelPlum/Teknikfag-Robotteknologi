ad520d2983333ed5b5aee002d890d6f014b84853
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-15 12:29:51 +0200

    Update main.cpp

--- Hello world/src/main.cpp
+++ Hello world/src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int i = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.println("Setup exiting");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
+  Serial.print("Counter: ");
+  Serial.print(i);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  i = i + 1;
+
+  delay(500);
 }
