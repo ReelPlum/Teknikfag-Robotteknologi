99c5ea6178998ae026b60bdff84d755e68d0c78c
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-18 13:24:59 +0200

    Servo setup & Servo Move

--- Useless Box/src/Motor.cpp
+++ Useless Box/src/Motor.cpp
@@ -1,13 +1,11 @@
 #include <Arduino.h>
 
-const uint8_t PIN_LED = 17;
-const int32_t PWM_CH = 0;
-const int32_t PWM_FREQ_HZ = 50;
-const int32_t PWM_RES_BITS = 8;
+const int32_t PWM_FREQ_HZ_Servo = 50;
+const int32_t PWM_RES_BITS_Servo = 8;
 const int32_t DLY_MS = 2;
 
-const int16_t MaxAngle = 170*2;
-const int32_t Counts = pow(2, PWM_RES_BITS);
+//const int16_t MaxAngle = 170*2;
+const int32_t SERVO_COUNTS = pow(2, PWM_RES_BITS_Servo);
 
 void setup() {
     // ledcAttachPin(PIN_LED, PWM_CH);
@@ -15,14 +13,19 @@ void setup() {
 }
 
 void Step_Move(){
-    
+
 }
 
-void Servo_Move(int16_t Angle, int32_t PWM_Channel) {
-    double percent = (Angle / MaxAngle);
-    int32_t value = Counts*0.05 + (Counts * percent * 0.05);
+void Setup_Servo(int32_t pwm_channel, uint8_t pin){
+    ledcAttachPin(pin, pwm_channel);
+    ledcSetup(pwm_channel, PWM_FREQ_HZ_Servo, PWM_RES_BITS_Servo);
+}
 
-    ledcWrite(PWM_Channel, value);
+void Servo_Move(int16_t angle, int16_t max_angle, int32_t pwm_channel) {
+    double percent = (angle / max_angle);
+    int32_t value = SERVO_COUNTS*0.05 + (SERVO_COUNTS * percent * 0.05);
+
+    ledcWrite(pwm_channel, value);
 }
 
 void loop() {
