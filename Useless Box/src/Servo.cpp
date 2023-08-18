41dd1d70d3c181bf33b720ac9c3a825557a38259
Frederik Bak <Frederikpreben@gmail.com>
2023-08-18 13:01:10 +0200

    Update Servo.cpp

--- Useless Box/src/Servo.cpp
+++ Useless Box/src/Servo.cpp
@@ -1,8 +1,15 @@
 #include <Arduino.h>
 
+const uint8_t PIN_LED = 17;
+const int32_t PWM_CH = 0;
+const int32_t PWM_FREQ_HZ = 50;
+const int32_t PWM_RES_BITS = 8;
+const int32_t DLY_MS = 2;
+
 
 void setup() {
-
+ledAttachPin(LED, CH)
+ledcSetup(CH, FREQ, BIT)
 }
 
 void loop() {
