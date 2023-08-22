d1571d7c66c0162fa7dd51855bb6d1634da0037c
Viktor Rask <142579885+TheAtomEmperor@users.noreply.github.com>
2023-08-22 13:09:58 +0200

    add SERVO_1

--- Test af motor/src/main.cpp
+++ Test af motor/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Servo.cpp>
 
+const int32_t SERVO_1 = 0;
 
 void setup() {
   Setup_Servo(0,18);
@@ -8,10 +9,10 @@ void setup() {
 }
 
 void loop() {
-  Servo_Move(90,MAX_ANGLE,0);
+  Servo_Move(90,MAX_ANGLE,SERVO_1);
   delay(2000);
-  Servo_Move(180,MAX_ANGLE,0);
+  Servo_Move(180,MAX_ANGLE,SERVO_1);
   delay(2000);
-  Servo_Move(0,MAX_ANGLE,0);
+  Servo_Move(0,MAX_ANGLE,SERVO_1);
 }
 
