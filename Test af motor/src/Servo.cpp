318ba7b3d8136dd866869129594a5b688f6bc54a
Viktor Rask <142579885+TheAtomEmperor@users.noreply.github.com>
2023-08-22 14:26:40 +0200

    test 2

--- Test af motor/src/Servo.cpp
+++ Test af motor/src/Servo.cpp
@@ -19,9 +19,8 @@ void Servo_Move(int16_t angle, int16_t max_angle, int32_t pwm_channel) {
       
     double percent = (angle / max_angle) * 100;
     double dc = map_float(percent, 0, 100, SERVO_T_LOW, SERVO_T_HIGH); // dc 2.5% -- 12.5%
-    Serial.println(dc)
     int32_t value = map_float(dc, SERVO_T_LOW, SERVO_T_HIGH, 0, pow(2, PWM_RES_BITS_Servo)); // 0 - 2^8
-    Serial.println(value)
+    
 
     //int32_t value = SERVO_COUNTS*0.05 + (SERVO_COUNTS * percent * 0.05);
 
