2c40c37d000b636dcd3f9aa30e48b3a43b9119bc
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-29 13:08:12 +0200

    Update main.cpp

--- Test af motor/src/main.cpp
+++ Test af motor/src/main.cpp
@@ -9,18 +9,11 @@ const uint8_t STEP_1_DIR_pin = 23;
 void setup()
 {
   Serial.begin(115200);
-  Setup_Servo(SERVO_1, 18);
-
   Setup_Stepper(STEP_1_PULSE_pin, STEP_1_DIR_pin);
 }
 
 void loop()
 {
-
-  Servo_Move(0, MAX_ANGLE, SERVO_1);
-  delay(2000);
-  Servo_Move(180, MAX_ANGLE, SERVO_1);
-  delay(2000);
 
   Move_Stepper(STEP_1_PULSE_pin, STEP_1_DIR_pin, 1, 200, 50);
 }
