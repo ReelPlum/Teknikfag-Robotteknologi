ed71c7b61af3c71d0e0883d8be7c194c1bb4128d
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-29 13:02:04 +0200

    Servo Class

--- Test af motor/src/Servo.cpp
+++ Test af motor/src/Servo.cpp
@@ -1,22 +1,63 @@
 #include <Servo.h>
 
 
-void Setup_Servo(int32_t pwm_channel, uint8_t pin)
+// void Setup_Servo(int32_t pwm_channel, uint8_t pin)
+// {
+//     //pinMode(pin, OUTPUT);
+//     ledcAttachPin(pin, pwm_channel);
+//     ledcSetup(pwm_channel, PWM_FREQ_HZ_Servo, PWM_RES_BITS_Servo);
+// }
+
+// void Servo_Move(double angle, double max_angle, int32_t pwm_channel)
+// {
+
+//     double percent = (angle / max_angle)*100;
+//     double dc = map_double(percent, 0, 100, 2.76, 12.75); // dc 2.5% -- 12.5%
+//     int32_t value = (int32_t)map_double(dc, 0, 100, 0, pow(2, PWM_RES_BITS_Servo)-1); //  
+//     Serial.printf("Percent: %.3f    DC: %.3f   value: %d\n", percent, dc, value);
+
+//     // int32_t value = SERVO_COUNTS*0.05 + (SERVO_COUNTS * percent * 0.05);
+
+//     ledcWrite(pwm_channel, value);
+// }
+
+class Servo
 {
-    //pinMode(pin, OUTPUT);
-    ledcAttachPin(pin, pwm_channel);
-    ledcSetup(pwm_channel, PWM_FREQ_HZ_Servo, PWM_RES_BITS_Servo);
-}
+    public:
+    void Servo::Setup(uint8_t pwm_channel, uint8_t pin, double t_low, double t_high, double servo_hz, int32_t pwm_freq_hz, int32_t pwm_res_bits, int32_t max_angle){
+        this->pwm_channel = pwm_channel;
+        this->pin = pin;
+        this->servo_hz = servo_hz;
+        this->t_low = (t_low/servo_hz)*100;
+        this->t_high = (t_high/servo_hz)*100;
+        this->pwm_freq_hz = pwm_freq_hz;
+        this->pwm_res_bits = pwm_res_bits;
+        this->max_angle = max_angle;
 
-void Servo_Move(double angle, double max_angle, int32_t pwm_channel)
-{
+        ledcAttachPin(pin, pwm_channel);
+        ledcSetup(pwm_channel, pwm_freq_hz, pwm_res_bits);
+    }
 
-    double percent = (angle / max_angle)*100;
-    double dc = map_double(percent, 0, 100, 2.76, 12.75); // dc 2.5% -- 12.5%
-    int32_t value = (int32_t)map_double(dc, 0, 100, 0, pow(2, PWM_RES_BITS_Servo)-1); //  
-    Serial.printf("Percent: %.3f    DC: %.3f   value: %d\n", percent, dc, value);
+    void Servo::Move(double angle)
+    {
 
-    // int32_t value = SERVO_COUNTS*0.05 + (SERVO_COUNTS * percent * 0.05);
+        double percent = (angle / this->max_angle)*100;
+        double dc = map_double(percent, 0, 100, this->t_low, this->t_high); // dc 2.5% -- 12.5%
+        int32_t value = (int32_t)map_double(dc, 0, 100, 0, pow(2, this->pwm_res_bits)-1); //  
+        Serial.printf("Percent: %.3f    DC: %.3f   value: %d\n", percent, dc, value);
 
-    ledcWrite(pwm_channel, value);
-}
+        // int32_t value = SERVO_COUNTS*0.05 + (SERVO_COUNTS * percent * 0.05);
+
+        ledcWrite(this->pwm_channel, value);
+    }
+
+    private:
+    int32_t pwm_channel;
+    uint8_t pin;
+    double max_angle;
+    double t_low;
+    double t_high;
+    int32_t pwm_freq_hz;
+    int32_t pwm_res_bits;
+    double servo_hz;
+};
