c90fda8b2c03c5de2e096f6eeeadce2eb1d49720
Viktor Rask <142579885+TheAtomEmperor@users.noreply.github.com>
2023-08-29 13:06:55 +0200

    måske fix

--- Test af motor/src/main.cpp
+++ Test af motor/src/main.cpp
@@ -3,15 +3,15 @@
 
 const int32_t SERVO_1 = 0;
 
-const uint8_t STEP_1_PULSE = 19;
-const uint8_t STEP_1_DIR = 20;
+const uint8_t STEP_1_PULSE_pin = 22;
+const uint8_t STEP_1_DIR_pin = 23;
 
 void setup()
 {
   Serial.begin(115200);
   Setup_Servo(SERVO_1, 18);
 
-  Setup_Stepper(STEP_1_PULSE, STEP_1_DIR);
+  Setup_Stepper(STEP_1_PULSE_pin, STEP_1_DIR_pin);
 }
 
 void loop()
@@ -22,5 +22,5 @@ void loop()
   Servo_Move(180, MAX_ANGLE, SERVO_1);
   delay(2000);
 
-  Move_Stepper(STEP_1_PULSE, STEP_1_DIR, 1, 200, 50);
+  Move_Stepper(STEP_1_PULSE_pin, STEP_1_DIR_pin, 1, 200, 50);
 }
