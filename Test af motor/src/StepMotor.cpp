08de3dd0d17383651597c57795b6d8e0dfce37fc
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-29 09:13:46 +0200

    Stepmotor funktionalitet

---
+++ Test af motor/src/StepMotor.cpp
@@ -0,0 +1,23 @@
+#include <StepMotor.h>
+
+void Setup_Stepper(uint8_t pulse_pin, uint8_t dir_pin)
+{
+    // ledcAttachPin(pin, pwm_channel);
+    // ledcSetup(pwm_channel, PWM_FREQ_HZ_STEP, PWM_RES_BITS_STEP);
+    pinMode(pulse_pin, OUTPUT);
+    pinMode(dir_pin, OUTPUT);
+}
+
+void Move_Stepper(uint8_t pulse_pin, uint8_t dir_pin, boolean dir = 1, uint32_t steps = 200, uint32_t speed = 100)
+{
+    //Speed er steps / second
+    
+    digitalWrite(dir_pin, dir);
+
+    for (int i = 0; i < steps; i++){
+        digitalWrite(pulse_pin, HIGH);
+        usleep(20);
+        digitalWrite(pulse_pin, LOW);
+        delay(1/speed);
+    }
+}
--- Test af motor/src/StepMotor.h
+++ Test af motor/src/StepMotor.h
@@ -1,5 +1,11 @@
 #include <Arduino.h>
 #include <math.h>
+#include <map.h>
 
-void Setup_Stepper(int32_t pwm_channel, uint8_t pin);
-void Move_Stepper();
+const double MAX_ANGLE = 180;
+const int32_t PWM_FREQ_HZ_STEP = 200;
+const int32_t PWM_RES_BITS_STEP = 12;
+const int32_t STEP_T_LOW = 552; //MIcroseconds
+const int32_t STEP_T_HIGH = 2550; //Microseconds
+
+void Setup_Stepper(uint8_t pulse_pin, uint8_t dir_pin);
