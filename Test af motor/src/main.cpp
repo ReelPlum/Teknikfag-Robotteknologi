6f5283df0f9478b5df7224dbe248334421e47ba4
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-29 13:24:56 +0200

    Update main.cpp

--- Test af motor/src/main.cpp
+++ Test af motor/src/main.cpp
@@ -1,19 +1,29 @@
+#include <global.h>
 #include <Servo.h>
 #include <StepMotor.h>
 
-const int32_t SERVO_1 = 0;
+const int32_t SERVO_PIN = 18;
 
 const uint8_t STEP_1_PULSE_pin = 22;
 const uint8_t STEP_1_DIR_pin = 23;
 
+Servo SERVO1;
+
 void setup()
 {
   Serial.begin(115200);
-  Setup_Stepper(STEP_1_PULSE_pin, STEP_1_DIR_pin);
+  //Setup_Stepper(STEP_1_PULSE_pin, STEP_1_DIR_pin);
+
+  SERVO1.Setup(0, SERVO_PIN, SERVO_T_LOW, SERVO_T_HIGH, 20*1000, PWM_FREQ_HZ_Servo, PWM_RES_BITS_Servo, MAX_ANGLE);
 }
 
 void loop()
 {
 
-  Move_Stepper(STEP_1_PULSE_pin, STEP_1_DIR_pin, 1, 200, 50);
+  //Move_Stepper(STEP_1_PULSE_pin, STEP_1_DIR_pin, 1, 200, 50);
+
+  SERVO1.Move(180);
+  delay(1000);
+  SERVO1.Move(0);
+  delay(1000);
 }
