f81a9e49be03ef9ec6b365da1df0eb31107894c4
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-29 11:24:56 +0200

    Stepper motor test

--- Test af motor/src/main.cpp
+++ Test af motor/src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <StepMotor.h>
 
 const int32_t SERVO_1 = 0;
-int32_t i = 1;
+
+const uint8_t STEP_1_PULSE = 19;
+const uint8_t STEP_1_DIR = 20;
 
 void setup()
 {
   Serial.begin(115200);
   Setup_Servo(SERVO_1, 18);
+
+  Setup_Stepper(STEP_1_PULSE, STEP_1_DIR);
 }
 
 void loop()
@@ -18,4 +23,5 @@ void loop()
   Servo_Move(180, MAX_ANGLE, SERVO_1);
   delay(2000);
 
+  Move_Stepper(STEP_1_PULSE, STEP_1_DIR, 1, 200, 50);
 }
