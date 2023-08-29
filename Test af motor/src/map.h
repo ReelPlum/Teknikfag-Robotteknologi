d19fcae5a8f5aa55ec2c98a6c78590b1c016f043
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-29 08:43:26 +0200

    map_float -> map_double
    Synes det var lidt træls

--- Test af motor/src/map.h
+++ Test af motor/src/map.h
@@ -1,3 +1,3 @@
 #include <math.h>
 
-double map_float(double x, double x_min, double x_max, double y_min, double y_max);
+double map_double(double x, double x_min, double x_max, double y_min, double y_max);
