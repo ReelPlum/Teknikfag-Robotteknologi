85a9fa9cfb4abf6dd638e0f0f89533cbaa259e70
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-29 13:46:04 +0200

    Test

--- Test af motor/src/StepMotor.cpp
+++ Test af motor/src/StepMotor.cpp
@@ -11,17 +11,17 @@ void StepMotor::Move(int32_t steps, uint32_t speed)
     //Speed er steps / second
 
     if (steps < 0){
-        digitalWrite(dir_pin, 1);
+        digitalWrite(this->dir_pin, 1);
         steps = -steps;
     }
     else{
-        digitalWrite(dir_pin, 0);
+        digitalWrite(this->dir_pin, 0);
     }
 
     for (int i = 0; i < steps; i++){
-        digitalWrite(pulse_pin, HIGH);
+        digitalWrite(this->pulse_pin, HIGH);
         usleep(10);
-        digitalWrite(pulse_pin, LOW);
+        digitalWrite(this->pulse_pin, LOW);
         int32_t delay_ms = (1.0/speed) * 1000;
 
         delay(delay_ms);
