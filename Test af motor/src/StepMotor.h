42046fc861913949ddd5134b018431cd50591935
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-08-29 09:18:38 +0200

    Fjern konstanter

--- Test af motor/src/StepMotor.h
+++ Test af motor/src/StepMotor.h
@@ -2,10 +2,5 @@
 #include <math.h>
 #include <map.h>
 
-const double MAX_ANGLE = 180;
-const int32_t PWM_FREQ_HZ_STEP = 200;
-const int32_t PWM_RES_BITS_STEP = 12;
-const int32_t STEP_T_LOW = 552; //MIcroseconds
-const int32_t STEP_T_HIGH = 2550; //Microseconds
-
 void Setup_Stepper(uint8_t pulse_pin, uint8_t dir_pin);
+void Move_Stepper(uint8_t pulse_pin, uint8_t dir_pin, boolean dir = 1, uint32_t steps = 200, uint32_t speed = 100);
