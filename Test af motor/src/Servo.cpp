eff2471dc943f8fbd852450518e76a0f406abf34
Viktor Rask <142579885+TheAtomEmperor@users.noreply.github.com>
2023-09-05 12:07:56 +0200

    Update Servo.cpp

--- Test af motor/src/Servo.cpp
+++ Test af motor/src/Servo.cpp
@@ -1,7 +1,7 @@
 #include <global.h>
 #include <Servo.h>
 
-void Servo::Setup(uint8_t pwm_channel, uint8_t pin, double t_low = SERVO_T_LOW, double t_high = SERVO_T_HIGH, double servo_hz = 50, int32_t pwm_freq_hz = PWM_FREQ_HZ_Servo, int32_t pwm_req_bits = PWM_RES_BITS_Servo, int32_t max_angle = MAX_ANGLE){
+void Servo::Setup(uint8_t pwm_channel, uint8_t pin, double t_low, double t_high , double servo_hz, int32_t pwm_freq_hz, int32_t pwm_req_bits , int32_t max_angle ){
     this->pwm_channel = pwm_channel;
     this->pin = pin;
     this->servo_hz = servo_hz;
