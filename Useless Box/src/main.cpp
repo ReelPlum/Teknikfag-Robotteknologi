7889fbc118ce8b61158863bb5fb37096c6ba4925
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-09-05 09:04:08 +0200

    Noter

--- Useless Box/src/main.cpp
+++ Useless Box/src/main.cpp
@@ -3,6 +3,18 @@
 #include <StepMotor.h>
 #include <random.h>
 
+/*
+Ideer til stages:
+
+1. Gå op og luk med det samme
+2. Gå op, vent og luk hurtigt
+3. Gå op, ned, op, ned, op og så luk
+4. Gå op og luk langsomt. Gå langsomt ned
+5. Gå op pause, sluk langsomt og gå langsomt ned.
+
+
+*/
+
 void setup() {
 }
 
