b04406d36bc881e45313f71d26a6389ab6221eb3
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-09-05 10:31:01 +0200

    Implemention af task

--- Useless Box/src/main.cpp
+++ Useless Box/src/main.cpp
@@ -25,6 +25,8 @@ const uint8_t STEPPER_PULSE_PIN;
 
 const uint8_t SWITCH_PIN;
 
+TaskHandle_t GAMETASK_HANDLE;
+
 int32_t NUM_OF_STAGES = 7; 
 
 int32_t Current_Stage = 0;
@@ -37,14 +39,14 @@ void start() {
     Started = true;
 }
 
-void setup() {
-    SERVO_1.Setup(SERVO_PWM, SERVO_PIN);
-    STEP_1.Setup(STEPPER_PULSE_PIN, STEPPER_DIR_PIN);
-
-    pinMode(SWITCH_PIN, INPUT);
-}
-
-void loop() {
+void game_task(void *args)
+{
+double loop_time_s = 0.01;
+  TickType_t xTimeIncrement = configTICK_RATE_HZ * loop_time_s;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  for(;;)
+  {
+    //Run game task here
     if (not Started){
         //Tjek hvis switchen er slået til. Start spillet hvis knappen er til.
         if (digitalRead(SWITCH_PIN) == HIGH){
@@ -57,7 +59,28 @@ void loop() {
 
     int32_t stage_num = random_int_range(0, NUM_OF_STAGES-1);
 
-
-
     Current_Stage++;
+
+    vTaskDelayUntil(&xLastWakeTime, xTimeIncrement);
+  }
+}
+
+void setup() {
+    SERVO_1.Setup(SERVO_PWM, SERVO_PIN);
+    STEP_1.Setup(STEPPER_PULSE_PIN, STEPPER_DIR_PIN);
+
+    pinMode(SWITCH_PIN, INPUT);
+
+    xTaskCreatePinnedToCore(
+    game_task,         
+    "Game Task",        
+    10000,               /* Stack size in words */
+    NULL,                /* Task input parameter */
+    2,                   /* Priority of the task from 0 to 25, higher number = higher priority */
+    &GAMETASK_HANDLE,   
+    1);                  /* core no */
+}
+
+void loop() {
+    
 }
