8250cbb09055862f4c873ace8c25d6e7e26b64dc
Viktor Rask <142579885+TheAtomEmperor@users.noreply.github.com>
2023-09-05 13:51:55 +0200

    El Finale

---
+++ Useless Box/src/ElFinale.cpp
@@ -0,0 +1,26 @@
+#include <ElFinale.h>
+const int32_t step_move = 100;
+
+void ElFinale(Servo servo, StepMotor step_motor)
+{
+    step_motor.Move(step_move/2,100);
+    delay(2000);
+    for (int i = 1; i < 181; i++)
+    {
+        servo.Move(i);
+        delay(50);
+    }
+    delay(1000);
+    step_motor.Move(step_move/2,100);
+    delay(1000);
+    step_motor.Move(-(step_move/2),100);
+    delay(2000);
+    for (int i = 180; i > 0; i -= 1)
+    {
+        servo.Move(i);
+        delay(50);
+    }
+    delay(1000);
+    step_motor.Move(-(step_move/2),100);
+    
+}
---
+++ Useless Box/src/ElFinale.h
@@ -0,0 +1,6 @@
+#include <global.h>
+#include <Servo.h>
+#include <StepMotor.h>
+#include <StageOne.h>
+
+void ElFinale(Servo servo, StepMotor step_motor);
