637a6e049ab77126ae11e3b01ac272fd2996981d
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-09-12 09:35:48 +0200

    fix stage four

--- Useless Box/src/StageFour.cpp
+++ Useless Box/src/StageFour.cpp
@@ -3,7 +3,6 @@
 const int32_t step_move = 100;
 int32_t counter = 0;
 
-
 void stage_4(Servo servo, StepMotor step_motor)
 {
     step_motor.Move(step_move / 2, 100);
@@ -16,40 +15,35 @@ void stage_4(Servo servo, StepMotor step_motor)
 void StageFour(Servo servo, StepMotor step_motor, int32_t current_round)
 {
     int32_t Tal = random(0, 3);
-    int32_t Round = random(6,9);
-    while (counter == 0)
+    if (current_round == 0)
     {
         stage_4(servo, step_motor);
-        counter++;
+        return;
     }
 
-    while (1 < counter < Round)
-    {     
-        counter++; 
-        if (Tal == 0)
-        {
-            StageOne(servo, step_motor);
-        }
-
-        if (Tal == 1)
-            ;
-        {
-            step_motor.Move(step_move, 100);
-            delay(1000);
-            step_motor.Move(-step_move, 100);
-        }
-
-        if (Tal == 2)
-            ;
-        {
-            step_motor.Move(step_move, 400);
-            delay(1000);
-            step_motor.Move(-step_move, 400);
-        }
-
-        if (Tal == 3)
-        {
-            stage_4(servo, step_motor);
-        }
+    if (Tal == 0)
+    {
+        StageOne(servo, step_motor);
     }
-}   
+
+    if (Tal == 1)
+        ;
+    {
+        step_motor.Move(step_move, 100);
+        delay(1000);
+        step_motor.Move(-step_move, 100);
+    }
+
+    if (Tal == 2)
+        ;
+    {
+        step_motor.Move(step_move, 400);
+        delay(1000);
+        step_motor.Move(-step_move, 400);
+    }
+
+    if (Tal == 3)
+    {
+        stage_4(servo, step_motor);
+    }
+}
