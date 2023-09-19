fc338d4b8d38e7a3f1caabb093f7dcc4d60df22e
Viktor Rask <142579885+TheAtomEmperor@users.noreply.github.com>
2023-09-19 13:18:40 +0200

    Update main.cpp

--- Test af motor/src/main.cpp
+++ Test af motor/src/main.cpp
@@ -2,11 +2,11 @@
 #include <Servo.h>
 #include <StepMotor.h>
 
-const int32_t SERVO_PIN = 18;
+const int32_t SERVO_PIN = 21;
 
 const uint8_t STEP_1_PULSE_pin = 22;
 const uint8_t STEP_1_DIR_pin = 23;
-
+int32_t count = 0;
 Servo SERVO1;
 
 void setup()
@@ -14,16 +14,15 @@ void setup()
   Serial.begin(115200);
   delay(200);
 
-
-  SERVO1.Setup(0, SERVO_PIN, SERVO_T_LOW, SERVO_T_HIGH, 20*1000, PWM_FREQ_HZ_Servo, PWM_RES_BITS_Servo, MAX_ANGLE);
+  SERVO1.Setup(0, SERVO_PIN, SERVO_T_LOW, SERVO_T_HIGH, 20 * 1000, PWM_FREQ_HZ_Servo, PWM_RES_BITS_Servo, MAX_ANGLE);
 }
 
 void loop()
 {
-  for (int i = 76; i == 180 ; i++)
-    {
-        SERVO1.Move(i);
-        delay(20);
-    }
-  
+  if (count == 0)
+  {
+    SERVO1.Move(75);
+    delay(20);
+    count++;
+  }
 }
