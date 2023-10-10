0ca4c31911cdcd981667c6e4e597ec4f2415f264
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-10-10 10:51:22 +0200

    class start

--- Motor med Webserver/src/DCMotor.h
+++ Motor med Webserver/src/DCMotor.h
@@ -9,70 +9,81 @@ double getData(char subtype);
 
 void init_dc();
 
-// class DCMotor{
-//     public:
-//     DCMotor(){};
+class DCMotor{
+    public:
+    DCMotor(){};
 
-//     void setup(int32_t pin_pid_loop, int32_t pin_enc_a,);
-//     double getData(char subtype);
-//     void update(double *paramValue, char subtype);
-//     void setPos(int32_t pos);
-//     void home();
+    void setup(int32_t pin_pid_loop, int32_t pin_enc_a, int32_t pin_enc_b, int32_t pin_limit_sw, int32_t pin_hbridge_ina, int32_t pin_hbridge_inb, int32_t pin_hbridge_pwm, int32_t pwm_ch, int32_t pwm_freq_hz, int32_t pwm_res_bits, int32_t pwm_min, int32_t pwm_max, double dt_s, double pid_max_ctrl_value, double min_ctrl_value, double max_ctrl_value);
+    double getData(char subtype);
+    void update(double *paramValue, char subtype);
+    void setPos(int32_t pos);
+    void home();
+    void setModePos(bool state);
 
+    void set_pos_kp(double val);
+    void set_pos_ki(double val);
+    void set_pos_kd(double val);
+    void set_vel_ki(double val);
 
-//     private:
-//     void motionTask(void *arg);
-//     void waitMove();
-//     void pidTask(void *arg);
+    double get_pos_kp();
+    double get_pos_ki();
+    double get_pos_kd();
+    double get_vel_ki();
 
-//     TaskHandle_t PidTaskHandle;
-//     TaskHandle_t MotionTaskHandle;
-//     ESP32Encoder encoder;
-//     Pid pidVel();
-//     Pid pidPos();
+    private:
+    void motionTask(void *arg);
+    void waitMove();
+    void pidTask(void *arg);
 
-//     //Pins
-//     int32_t PIN_PID_LOOP = 17;
-//     int32_t PIN_ENC_A = 26;
-//     int32_t PIN_ENC_B = 27;
+    TaskHandle_t PidTaskHandle;
+    TaskHandle_t MotionTaskHandle;
+    ESP32Encoder encoder;
 
-//     int32_t PIN_LIMIT_SW = 25;
+    //Pins
+    int32_t PIN_PID_LOOP = 17;
+    int32_t PIN_ENC_A = 26;
+    int32_t PIN_ENC_B = 27;
 
-//     int32_t PIN_HBRIDGE_INA = 18; // hbridge INA pin
-//     int32_t PIN_HBRIDGE_INB = 19; // hbridge INB pin
-//     int32_t PIN_HBRIDGE_PWM = 23; // hbridge PWM pin
+    int32_t PIN_LIMIT_SW = 25;
 
-//     //PWM
-//     const int32_t PWM_CH = 0;                             // 0 - 7(15?)  = 8(16?) channels
-//     const int32_t PWM_FREQ_HZ = 19500;                    // 1Hz to 40MHz
-//     const int32_t PWM_RES_BITS = 12;                      // 1 to 16(20) bits
-//     const int32_t PWM_MIN = 0;                            // minimum pwm value
-//     const int32_t PWM_MAX = pow(2, PWM_RES_BITS) - 1;     // maximum pwm value
+    int32_t PIN_HBRIDGE_INA = 18; // hbridge INA pin
+    int32_t PIN_HBRIDGE_INB = 19; // hbridge INB pin
+    int32_t PIN_HBRIDGE_PWM = 23; // hbridge PWM pin
 
-//     // PID SETUP
-//     const double DT_S = 0.001;
-//     const double PID_MAX_CTRL_VALUE = 4000;
-//     const double MIN_CTRL_VALUE = -100;
-//     const double MAX_CTRL_VALUE = 100;
+    //PWM
+    int32_t PWM_CH = 0;                             // 0 - 7(15?)  = 8(16?) channels
+    int32_t PWM_FREQ_HZ = 19500;                    // 1Hz to 40MHz
+    int32_t PWM_RES_BITS = 12;                      // 1 to 16(20) bits
+    int32_t PWM_MIN = 0;                            // minimum pwm value
+    int32_t PWM_MAX = pow(2, PWM_RES_BITS) - 1;     // maximum pwm value
 
-//     //PID
-//     int32_t LedState = 0;
-//     int32_t SliderVal = 0;
-//     double KpVal = 3.1415;
-//     double KiVal = 2.71;
-//     double KdVal = 42.0;
-//     double KdVelVal = 10.0;
+    // PID SETUP
+    double DT_S = 0.001;
+    double PID_MAX_CTRL_VALUE = 4000;
+    double MIN_CTRL_VALUE = -100;
+    double MAX_CTRL_VALUE = 100;
 
-//     const double integration_threshold = 200;
+    Pid pidVel();
+    Pid pidPos();
 
-//     volatile double req_pos = 100000;
-//     volatile double req_vel = 3000;
-//     volatile int64_t current_pos;
-//     volatile double current_vel;
-//     volatile double max_vel = 5000;
+    //PID
+    int32_t LedState = 0;
+    int32_t SliderVal = 0;
+    double KpVal = 3.1415;
+    double KiVal = 2.71;
+    double KdVal = 42.0;
+    double KdVelVal = 10.0;
 
-//     double ctrl_pos;
-//     double ctrl_vel;
+    const double integration_threshold = 200;
 
-//     bool mode_pos = true;
-// }
+    volatile double req_pos = 100000;
+    volatile double req_vel = 3000;
+    volatile int64_t current_pos;
+    volatile double current_vel;
+    volatile double max_vel = 5000;
+
+    double ctrl_pos;
+    double ctrl_vel;
+
+    bool mode_pos = true;
+};
