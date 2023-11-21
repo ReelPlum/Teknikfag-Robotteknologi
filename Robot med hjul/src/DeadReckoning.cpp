718ba11b8d0473fb1046f55281c8ad30577c26db
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-11-21 12:22:01 +0100

    Dead Reckoning
    Implementeret dead reckoning fra https://tstaarhustech-my.sharepoint.com/personal/josa_aarhustech_dk/_layouts/15/guestaccess.aspx?guestaccesstoken=4ROXg99N9Zv%2bp4M1xdXR2WqQvHVBsOWq0LxpW4pSbxw%3d&docid=2_05e64edf631154c53a139ce3525f9ecca&rev=1

---
+++ Robot med hjul/src/DeadReckoning.cpp
@@ -0,0 +1,24 @@
+#include <math.h>
+
+double CalculateAcceleration(double angleAcceleration, double wheelRadius)
+{
+    return angleAcceleration*wheelRadius;
+}
+
+double CalculateVelocity(double angleVelocity, double wheelRadius)
+{
+    return angleVelocity * wheelRadius;
+}
+
+double GetPosition(double *currentX, double *currentY, double *currentAngle, double aR, double aL, double wR, double wL, double b, double t)
+{
+    double A = (aR + aL) / 2;
+    double B = (wR + wL) / 2;
+    double C = (aR - aL) / (2*b);
+    double D = (wR - wL) / b;
+
+    *currentX = *currentX + (A * t + B)*cos(C* (t*t) + D*t + *currentAngle);
+    *currentY = *currentY + (A*t+ B)*sin(C* (t*t) + D*t + *currentAngle);
+
+    *currentAngle = C*(t*t)+D*t + *currentAngle;
+}
