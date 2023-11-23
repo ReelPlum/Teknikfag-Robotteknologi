955d9bc0a50017ed7bf266bdabb2add1ee742e61
Plum <65305525+ReelPlum@users.noreply.github.com>
2023-11-23 09:59:55 +0100

    Update main.cpp

--- Robot med hjul/src/main.cpp
+++ Robot med hjul/src/main.cpp
@@ -4,6 +4,12 @@
 
 DCMotor motor(false, 17, 26, 27, 1, 18, 19, 23, 1, 19500, 12, 0.1, 4000, -100, 100, 100, 200, 1990);
 
+/*
+For at bruge motor classen starter man den med DCMotor motor som set ovenover. Kig ind i files DCMotor.h for at se hvordan den virker.
+Der er ikke implementeret positions styring endnu men hvis position_mode er sat til false vil den bare køre (tror jeg)
+For at få motoren til at starte kaldes der motor.init();
+*/
+
 void setup(){
   Serial.begin(115200);
 
