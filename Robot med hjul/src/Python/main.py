383886a46f2ee9b775e645fdd06939959d80178e
Frederik Bak <Frederikpreben@gmail.com>
2024-01-19 12:24:52 +0100

    Movement med wasd og pil taster virker

--- Robot med hjul/src/Python/main.py
+++ Robot med hjul/src/Python/main.py
@@ -3,6 +3,7 @@ import websocket
 import keyboard
 import _thread as thread
 import time
+from math import *
 
 class Interface(Frame):
     def __init__(self, master):
@@ -31,7 +32,7 @@ class Interface(Frame):
         self.X = 0
 
         while True:
-            time.sleep(0.5)
+            time.sleep(0.2)
             x = 0
             y = 0
             Controllist = ["w","a","s","d"]
@@ -48,16 +49,26 @@ class Interface(Frame):
             if keyboard.is_pressed(Controllist[3]) or keyboard.is_pressed("right_arrow"):
                 x = x + 1
                     
+
+            l = sqrt(x**2 + y**2)
+            print(l)
+            if x != 0:
+                x = x/l
+
+            if y != 0:
+                y = y/l
+
+
             message = (f"x:{x}, y:{y}]")
             print(message)
 
             if self.X != x:
                 self.X = x
-                self.ws.send(f'x:{x}')
+                self.ws.send(f'rotate:{round(x*1000)}')
 
             if self.Y != y:
                 self.Y = y
-                self.ws.send(f'y:{y}')
+                self.ws.send(f'forward:{round(y*1000)}')
 
 
 
